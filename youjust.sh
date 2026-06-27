42edeef51277097d7375f8db6ed0b5288fba9bf3
AramCZ <pigeon-quarter03@icloud.com>
2026-06-27 19:05:52 +0200

    Fixing Go Into

--- youjust.sh
+++ youjust.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
 _youjust_completion() {
-    local cur="${COMP_WORDS[1]}"
+    local cur="${COMP_WORDS[COMP_CWORD]}"
 
     COMPREPLY=($(compgen -W "
         install remove update upgrade
-        createfile createfolder delete
-        showfile list
+        create file create folder delete
+        show file list
         copy move rename
         showsystem
         see ip
         see processes
         see space
         see whereami
+        go into
+        ping kill
         help gethelp
         clear reboot shutdown
     " -- "$cur"))
+}
+
+youjust() {
+    local output
+    output=$(command youjust.py "$@")
+
+    # trim whitespace
+    output=$(echo "$output" | xargs)
+
+    # execute cd if returned
+    if [[ "$output" == cd* ]]; then
+        cd "${output#cd }"
+    else
+        echo "$output"
+    fi
 }
 
 complete -F _youjust_completion youjust
