35909494ed55217b963ec0567017ab29aa9e5252
AramCZ <pigeon-quarter03@icloud.com>
2026-06-28 11:47:02 +0200

    Fix

--- youjust.sh
+++ youjust.sh
@@ -3,8 +3,12 @@ youjust() {
 
     output=$(command youjust "$@")
 
+    # trim whitespace safely
+    output=$(echo "$output" | sed 's/^[ \t]*//;s/[ \t]*$//')
+
+    # execute cd in current shell if returned
     if [[ "$output" == cd\ * ]]; then
-        builtin cd "${output:3}"
+        builtin cd "${output#cd }"
         return
     fi
 
